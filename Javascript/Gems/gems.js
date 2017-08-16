2f871bc61e4e8122fe8f9f9f20b658f5e764e177
Manuel Rodríguez <m.rodriguez@pymasde.es>
2017-08-16 18:44:53 +0200

    Gems Grid

--- Javascript/Gems/gems.js
+++ Javascript/Gems/gems.js
@@ -5,7 +5,6 @@ var context = canvas.getContext("2d");
 window.addEventListener("mousedown", checkMouseDown, true);
 window.addEventListener("mousemove", checkMouseDown, true);
 
-var es = 80;
 var myGrid = new Grid(5, 5);
 
 setInterval(function() {
@@ -14,19 +13,21 @@ setInterval(function() {
 }, 1000/FPS);
 
 function update() {
-	//console.log("Updating..." + Math.random())
-
 }
 
 function draw() {
-	console.log(myGrid)
-	for (var i = myGrid.length - 1; i >= 0; i--) {
-		myGrid[i].Box.draw(context);
-	}
+	context.fillStyle = "white";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	myGrid.get(context);
 }
 
 function checkMouseDown(e) {
 	//console.log(e);l
+}
+
+function randomColor() {
+	this.colors = Array('red', 'green', 'blue', 'yellow');
+	return this.colors[Math.floor(Math.random()*this.colors.length)]
 }
 
 function Grid(w, h) {
@@ -34,26 +35,31 @@ function Grid(w, h) {
 	this.h = h || 5;
 	this.elements=[];
 
-	for (var i = 0; i <= w; i++) {
-		for (var j = 0; j <= h; j++) {
-			this.elements[i, j] = new Box(i*80,j*80,80,80);
-		}
+	for(var y = 0; y < h; y++){
+    	this.elements[y] = [];    
+    	for(var x = 0; x < w; x++){ 
+        	this.elements[y][x] = new Box(x*80, y*80, 80, 80, randomColor());
+    	}    
 	}
 }
 
-Grid.prototype.fill = function(w, h) {
-	// body...
+Grid.prototype.get = function(context) {
+	for(var y = 0; y < this.h; y++){
+    	for(var x = 0; x < this.w; x++){ 
+        	this.elements[y][x].draw(context);
+    	}    
+	}
 };
 
 function Box(x, y, w, h, fill) {
-	this.x = x || 80;
-	this.y = y || 80;
-	this.w = w || 80;
-	this.h = h || 80;
-	this.fill = fill || "red";
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.fill = fill || randomColor();
 }
 
 Box.prototype.draw = function(context) {
 	context.fillStyle = this.fill;
-	context.fillRect(this.x, this.y, this.w-2, this.h-2);
+	context.fillRect(this.x+2, this.y+2, this.w-4, this.h-4);
 };
